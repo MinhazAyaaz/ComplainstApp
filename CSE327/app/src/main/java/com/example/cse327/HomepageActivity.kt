5b9c0b7e169d0e1ee34acd113df8c4f5eabc0d71
Minhazul Abedin <37115237+LancelotGFX@users.noreply.github.com>
2022-03-07 23:32:29 +0600

    Implemented functionality for the checkboxes

--- CSE327/app/src/main/java/com/example/cse327/HomepageActivity.kt
+++ CSE327/app/src/main/java/com/example/cse327/HomepageActivity.kt
@@ -3,7 +3,9 @@ package com.example.cse327
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -19,5 +21,36 @@ import androidx.appcompat.app.AppCompatActivity
                 startActivity(intent)
             }
 
+        }
+
+        fun onCheckboxClicked(view: View) {
+            if (view is CheckBox) {
+                val checked: Boolean = view.isChecked
+
+                when (view.id) {
+                    R.id.checkBox -> {
+                        if (checked) {
+                            // Put some meat on the sandwich
+                        } else {
+                            // Remove the meat
+                        }
+                    }
+                    R.id.checkBox2 -> {
+                        if (checked) {
+                            // Cheese me
+                        } else {
+                            // I'm lactose intolerant
+                        }
+                    }
+                    R.id.checkBox3 -> {
+                        if (checked) {
+                            // Cheese me
+                        } else {
+                            // I'm lactose intolerant
+                        }
+                    }
+                    // TODO: Veggie sandwich
+                }
+            }
         }
     }
