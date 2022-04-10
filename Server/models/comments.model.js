d4b20db26d3238735a56269835e349b27b24aff7
Emon Sarker <emon.sarker@northsouth.edu>
2022-04-10 23:49:48 +0600

    modelled comment db; created reply component

---
+++ Server/models/comments.model.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, Sequelize) => {
+    const Comments = sequelize.define("comments", {
+      commentId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      comment: {
+        type: Sequelize.STRING
+      },
+      user: {
+        type: Sequelize.STRING
+      }
+    });
+  
+    return Tutorial;
+
+  };
+
+  
---
+++ Server/routes/comment.routes.js
@@ -0,0 +1,70 @@
+const { verifySignUp } = require("../middleware");
+const controller = require("../controllers/comments.controller");
+const { authJwt } = require("../middleware");
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+  app.post(
+    "/signup",
+    [
+      verifySignUp.checkDuplicateNsuidOrEmail,
+      verifySignUp.checkId,
+      verifySignUp.checkname,verifySignUp.checkemail,verifySignUp.checkpassword,verifySignUp.checkrole
+    ],
+    controller.signup
+  );
+
+  app.post("/login",   [
+   verifySignUp.checkId,
+  ],controller.login);
+
+  app.post(
+    "/Gsignup",
+    controller.GoogleSignup
+  );
+
+  app.get(
+    "/users",
+    [authJwt.verifyToken] ,
+    controller.findAll
+  );
+  app.get(
+    "/userswithstatus",
+    [authJwt.verifyToken] ,
+    controller.findUserWithStatus
+  );
+
+  app.get(
+    "/againstusers",
+    [authJwt.verifyToken] ,
+    controller.findUserToComplainAgainst
+  );
+
+  app.get(
+    "/findAll",
+    [authJwt.verifyToken] ,
+    controller.findAll
+  );
+
+  app.get(
+    "/idStatus",
+    [authJwt.verifyToken] ,
+    controller.findID
+  );
+
+  app.get(
+    "/confirmation/:token",
+    controller.update
+  );
+
+  app.post(
+    "/uploadId",
+    [authJwt.verifyToken],
+    controller.uploadId
+  );
+};
