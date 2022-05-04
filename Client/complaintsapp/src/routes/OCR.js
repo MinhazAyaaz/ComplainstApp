b6d4d3c9db2737e244f5bd2fadff46de39cf2189
Emon Sarker <emon.sarker@northsouth.edu>
2022-05-04 23:55:00 +0600

    writing data extraction algo

--- Client/complaintsapp/src/routes/OCR.js
+++ Client/complaintsapp/src/routes/OCR.js
@@ -3,6 +3,7 @@ import { createWorker } from "tesseract.js";
 
 function App() {
   const [ocr, setOcr] = useState("");
+  const [ocr2, setOcr2] = useState("");
   const [imageData, setImageData] = useState(null);
   const worker = createWorker({
     logger: (m) => {
@@ -17,8 +18,38 @@ function App() {
     const {
       data: { text },
     } = await worker.recognize(imageData);
-    var text2 = text.split(" ")
-    setOcr(text2);
+    var text2 = text
+    // text2 = text.replace(/\\n/g, " ");
+    text2 = text2.split('\n')
+    // text2 = text2.toString()
+    
+    console.log(text2)
+    var finalString = ""
+    var tempString
+    // text2.forEach((word)=>{
+    //     if(word.length == 10){
+    //         finalString = word
+    //     }
+    // })
+    for(var i=0; i<text2.length;i++){
+        if(text2[i].length >= 10){
+            tempString = text2[i]
+            tempString = Array.from(tempString)
+            for(var j=0; j<tempString.length; j++){
+                if(!isNaN(tempString[j])){
+                    finalString = finalString + tempString[j]
+                }
+            }
+            
+            console.log(finalString)
+        
+        }
+        if(finalString.length == 10){
+            break
+        }
+    }
+    setOcr(finalString);
+    setOcr2(text)
   };
 
   useEffect(() => {
@@ -49,8 +80,9 @@ function App() {
         />
       </div>
       <div className="display-flex">
-        <img src={imageData} alt="" srcset="" />
+        <img src={imageData} alt="" srcset="" className="ocrimg" />
         <p>{ocr}</p>
+        <p>{ocr2}</p>
       </div>
     </div>
   );
